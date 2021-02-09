d7e14b5aeb3477c2f4d35c792f1f51e11fa52f4c
atalay <atalaykeskin@gmail.com>
2021-02-09 20:40:57 +0300

    Designer.cs is added in ConsolUI

---
+++ ConsoleApp/Designer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleUI
+{
+    public static class Designer
+    {
+        #region Console Table
+        public static int tableWidth = 130;
+
+        public static void Baslik(string baslik)
+        {
+            Console.WriteLine("");
+            PrintLine();
+            PrintRow(baslik);
+        }
+        public static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        public static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+            
+            Console.WriteLine(row);
+            PrintLine();
+        }
+
+        public static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        #endregion
+
+        #region Maximizer
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+        public static IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+        #endregion
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,41 +3,38 @@ using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 
-namespace ConsoleApp
+namespace ConsoleUI
 {
     class Program
     {
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            
-            Console.WriteLine("-------------Tüm Liste---------------------");
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            Designer.ShowWindow(Designer.ThisConsole, 3);
+
+            Console.Clear();
+            Designer.Baslik("Tümünü Liste");
+            Designer.PrintRow("Car ID", "Brand Name", "Color Name", "Model Year", "Daily Price", "Description");
+
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine("Id:{0,-3:D} PlakaNo:{1} DailyPrice:{0,-4:D} Description:{3} ModelYear:{4}",
-                    car.Id, car.PlakaNo, car.DailyPrice, car.Description, car.ModelYear.Year);
+                Designer.PrintRow(car.Id.ToString(), brandManager.GetById(car.BrandId).Name, colorManager.GetById(car.ColorId).Name, car.ModelYear.ToString(), car.DailyPrice.ToString(), car.Description);
             }
 
-
-            Car car1 = carManager.GetById(7);
-            Console.WriteLine($"-------------{car1.Id}-Nolu Araba Bilgileri--------------------");
-            Console.WriteLine($"Id:{car1.Id} " +
-                              $"PlakaNo:{car1.PlakaNo} " +
-                              $"DailyPrice:{car1.DailyPrice} " +
-                              $"Description:{car1.Description} " +
-                              $"ModelYear:{car1.ModelYear.Year}");
-
-            //Console.WriteLine($"-------------Sisteme Araba Ekleme--------------------");
-            //carManager.Add(new Car
-            //{
-            //    BrandId = 1,
-            //    ColorId = 1,
-            //    Name = "Renault Megan",
-            //    PlakaNo = "34AS245",
-            //    DailyPrice = 150,
-            //    Description = "3 teker",
-            //    ModelYear = new DateTime(2019, 02, 12)
-            //});
+            Designer.Baslik("Yeni Araba Ekleme");
+            carManager.Add(new Car
+            {
+                BrandId = 1,
+                ColorId = 1,
+                Name = "Renault Megan",
+                PlakaNo = "34AS245",
+                DailyPrice = 150,
+                Description = "3 teker",
+                ModelYear = new DateTime(2019, 02, 12)
+            });
 
             //Console.WriteLine($"-------------Araba Güncelleme--------------------");
             //carManager.Update(new Car
@@ -57,58 +54,60 @@ namespace ConsoleApp
             //    Id = 2
             //});
 
-            Console.WriteLine("-------------MarkayaId'ye Göre Liste---------------------");
-            foreach (var car in carManager.GetCarsByBrandId(1))
-            {
-                Console.WriteLine($"Id:{car.Id} " +
-                                  $"PlakaNo:{car.PlakaNo} " +
-                                  $"DailyPrice:{car.DailyPrice} " +
-                                  $"Description:{car.Description} " +
-                                  $"ModelYear:{car.ModelYear.Year}");
-            }
+            //Console.WriteLine("-------------MarkayaId'ye Göre Liste---------------------");
+            //foreach (var car in carManager.GetCarsByBrandId(1))
+            //{
+            //    Console.WriteLine($"Id:{car.Id} " +
+            //                      $"PlakaNo:{car.PlakaNo} " +
+            //                      $"DailyPrice:{car.DailyPrice} " +
+            //                      $"Description:{car.Description} " +
+            //                      $"ModelYear:{car.ModelYear.Year}");
+            //}
 
 
 
-            Console.WriteLine("-------------ColorId'ye Göre Liste---------------------");
-            foreach (var car in carManager.GetCarsByColorId(1))
-            {
-                Console.WriteLine($"Id:{car.Id} " +
-                                  $"PlakaNo:{car.PlakaNo} " +
-                                  $"DailyPrice:{car.DailyPrice} " +
-                                  $"Description:{car.Description} " +
-                                  $"ModelYear:{car.ModelYear.Year}");
-            }
+            //Console.WriteLine("-------------ColorId'ye Göre Liste---------------------");
+            //foreach (var car in carManager.GetCarsByColorId(1))
+            //{
+            //    Console.WriteLine($"Id:{car.Id} " +
+            //                      $"PlakaNo:{car.PlakaNo} " +
+            //                      $"DailyPrice:{car.DailyPrice} " +
+            //                      $"Description:{car.Description} " +
+            //                      $"ModelYear:{car.ModelYear.Year}");
+            //}
 
-            Console.WriteLine("-------------(100TL-200TL)Fiyat aralığına Göre Liste---------------------");
-            foreach (var car in carManager.GetByDailyPrice(100,400))
-            {
-                Console.WriteLine($"Id:{car.Id} " +
-                                  $"PlakaNo:{car.PlakaNo} " +
-                                  $"DailyPrice:{car.DailyPrice} " +
-                                  $"Description:{car.Description} " +
-                                  $"ModelYear:{car.ModelYear.Year}");
-            }
+            //Console.WriteLine("-------------(100TL-200TL)Fiyat aralığına Göre Liste---------------------");
+            //foreach (var car in carManager.GetByDailyPrice(100,400))
+            //{
+            //    Console.WriteLine($"Id:{car.Id} " +
+            //                      $"PlakaNo:{car.PlakaNo} " +
+            //                      $"DailyPrice:{car.DailyPrice} " +
+            //                      $"Description:{car.Description} " +
+            //                      $"ModelYear:{car.ModelYear.Year}");
+            //}
 
 
-            Console.WriteLine("-------------2020 Model Arabaları Liste---------------------");
-            foreach (var car in carManager.GetByModelYear(2020))
-            {
-                Console.WriteLine($"Id:{car.Id} " +
-                                  $"PlakaNo:{car.PlakaNo} " +
-                                  $"DailyPrice:{car.DailyPrice} " +
-                                  $"Description:{car.Description} " +
-                                  $"ModelYear:{car.ModelYear.Year}");
-            }
+            //Console.WriteLine("-------------2020 Model Arabaları Liste---------------------");
+            //foreach (var car in carManager.GetByModelYear(2020))
+            //{
+            //    Console.WriteLine($"Id:{car.Id} " +
+            //                      $"PlakaNo:{car.PlakaNo} " +
+            //                      $"DailyPrice:{car.DailyPrice} " +
+            //                      $"Description:{car.Description} " +
+            //                      $"ModelYear:{car.ModelYear.Year}");
+            //}
 
-            Console.WriteLine("-------------Detay Tüm Liste---------------------");
-            foreach (var car in carManager.GetCarDetails())
-            {
-                Console.WriteLine($"Id:{car.Id} " +
-                                  $"Plaka No:{car.PlakaNo} " +
-                                  $"Brand:{car.BrandName} " +
-                                  $"Color Name:{car.ColorName} " +
-                                  $"Daily Price:{car.DailyPrice}");
-            }
+            //Console.WriteLine("-------------Detay Tüm Liste---------------------");
+            //foreach (var car in carManager.GetCarDetails())
+            //{
+            //    Console.WriteLine($"Id:{car.Id} " +
+            //                      $"Plaka No:{car.PlakaNo} " +
+            //                      $"Brand:{car.BrandName} " +
+            //                      $"Color Name:{car.ColorName} " +
+            //                      $"Daily Price:{car.DailyPrice}");
+            //}
+
+            Console.ReadKey();
         }
     }
 }
